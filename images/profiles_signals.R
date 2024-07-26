4e4db706192f2c796e1a67bb0b3bc0d0074f749b
Yuhang (Tom) Lin <yhlin@iastate.edu>
2024-07-27 00:31:46 +0800

    Revisions and more plots

---
+++ images/profiles_signals.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+profiles <- read_csv("~/Documents/GitHub/Wirecuts/profiles/profiles-T1AW-LI-R1.csv", show_col_types = FALSE)
+profiles %>%
+  ggplot(aes(x = x, y = value)) +
+  geom_line(color = "#003A70") +
+  theme_bw() +
+  xlab(expression(paste("x (", mu, "m)"))) +
+  ylab(expression(paste("profile value (", mu, "m)")))
+ggsave(here::here("images/T1AW-LI-R1-profiles-plot.png"), width = 6, height = 2)
+
+
+signals <- read_csv("~/Documents/GitHub/Wirecuts/data-manual/wire-signatures-400-40.csv", show_col_types = FALSE)
+signals %>%
+  filter(id == "T1AW-LI-R1") %>%
+  ggplot(aes(x = x, y = sig)) +
+  geom_line(aes(color = "filtered")) +
+  geom_line(aes(x = x, y = raw_sig, color = "raw"), alpha = 0.6) +
+  theme_bw() +
+  xlab(expression(paste("x (", mu, "m)"))) +
+  ylab(expression(paste("signal value (", mu, "m)"))) +
+  labs(color = "signal") +
+  scale_color_manual(values = c(raw = "#40B4E5", filtered = "#003A70")) +
+  theme(legend.position = "bottom")
+ggsave(here::here("images/T1AW-LI-R1-signals-plot.png"), width = 6, height = 2.5)
